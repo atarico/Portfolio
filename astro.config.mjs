2e5745d4e39f4664a90d750dbc00f0341dfdb801
atarico <atarico9@gmail.com>
2026-03-31 03:10:31 -0300

    feat: migrate contact form to web3forms

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site:   'https://atarico.dev',
-  output: 'server',
+  output: 'static',
   adapter: vercel(),
   integrations: [sitemap()],
   vite: {
