4b101e469739dcb5526ad8148d29d194cc513004
atarico <atarico9@gmail.com>
2026-03-31 02:37:21 -0300

    fix: use output server for vercel SSR support

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site:   'https://atarico.dev',
-  output: 'static',
+  output: 'server',
   adapter: vercel(),
   integrations: [sitemap()],
   vite: {
